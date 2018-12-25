c098570f8113ca8c740b01477abb64c61dd18bee
pavankatari <pavan.katari.4u@gmail.com>
2018-12-25 12:32:40 +0000

    b

--- init/init_santoni.cpp
+++ init/init_santoni.cpp
@@ -1,7 +1,6 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2017-2018 The LineageOS Project.
+   Copyright (c) 2016, The CyanogenMod Project
+   Copyright (c) 2017, The Lineage Project
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -28,6 +27,7 @@
  */
 
 #include <fcntl.h>
+#include <stdlib.h>
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -38,61 +38,80 @@
 #include "vendor_init.h"
 #include "property_service.h"
 
-typedef struct {
-    char const *heapstartsize;
-    char const *heapgrowthlimit;
-    char const *heapsize;
-    char const *heapminfree;
-    char const *heapmaxfree;
-} dalvikprop_t;
+char const *heapstartsize;
+char const *heapgrowthlimit;
+char const *heapsize;
+char const *heapminfree;
+char const *heapmaxfree;
+char const *large_cache_height;
+
+static void init_alarm_boot_properties()
+{
+    int boot_reason;
+    FILE *fp;
+
+    fp = fopen("/proc/sys/kernel/boot_reason", "r");
+    fscanf(fp, "%d", &boot_reason);
+    fclose(fp);
+
+    /*
+     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+     * For existing PMIC chips, the following mapping applies
+     * for the value of boot_reason:
+     *
+     * 0 -> unknown
+     * 1 -> hard reset
+     * 2 -> sudden momentary power loss (SMPL)
+     * 3 -> real time clock (RTC)
+     * 4 -> DC charger inserted
+     * 5 -> USB charger inserted
+     * 6 -> PON1 pin toggled (for secondary PMICs)
+     * 7 -> CBLPWR_N pin toggled (for external power supply)
+     * 8 -> KPDPWR_N pin toggled (power key pressed)
+     */
+     if (boot_reason == 3) {
+        android::init::property_set("ro.alarm_boot", "true");
+     } else {
+        android::init::property_set("ro.alarm_boot", "false");
+     }
+}
 
 void check_device()
 {
-=======
-static void property_override(char const prop[], char const value[]) {
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-static void check_device(dalvikprop_t* dprop) {
     struct sysinfo sys;
 
     sysinfo(&sys);
 
-    if (dprop == nullptr)
-        return;
-
     if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xxhdpi-4096-dalvik-heap.mk
-        dprop->heapstartsize = "16m";
-        dprop->heapgrowthlimit = "256m";
-        dprop->heapsize = "512m";
-        dprop->heapminfree = "4m";
-        dprop->heapmaxfree = "8m";
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heapminfree = "4m";
+        heapmaxfree = "8m";
+	large_cache_height = "2048";
     } else if (sys.totalram > 2048ull * 1024 * 1024) {
         // from - phone-xxhdpi-3072-dalvik-heap.mk
-        dprop->heapstartsize = "8m";
-        dprop->heapgrowthlimit = "288m";
-        dprop->heapsize = "768m";
-        dprop->heapminfree = "512k";
-        dprop->heapmaxfree = "8m";
+        heapstartsize = "8m";
+        heapgrowthlimit = "288m";
+        heapsize = "768m";
+        heapminfree = "512k";
+	heapmaxfree = "8m";
+        large_cache_height = "1024";
     } else {
         // from - phone-xxhdpi-2048-dalvik-heap.mk
-        dprop->heapstartsize = "16m";
-        dprop->heapgrowthlimit = "192m";
-        dprop->heapsize = "512m";
-        dprop->heapminfree = "2m";
-        dprop->heapmaxfree = "8m";
+        heapstartsize = "16m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heapminfree = "2m";
+        heapmaxfree = "8m";
+        large_cache_height = "1024";
    }
 }
 
 void vendor_load_properties()
 {
+    init_alarm_boot_properties();
     check_device();
 
     android::init::property_set("dalvik.vm.heapstartsize", heapstartsize);
@@ -101,37 +120,17 @@ void vendor_load_properties()
     android::init::property_set("dalvik.vm.heaptargetutilization", "0.75");
     android::init::property_set("dalvik.vm.heapminfree", heapminfree);
     android::init::property_set("dalvik.vm.heapmaxfree", heapmaxfree);
-=======
-static void init_target_properties() {
-    std::ifstream fin;
-    std::string buf;
 
-    std::string product = GetProperty("ro.product.name", "");
-    if (product.find("santoni") == std::string::npos)
-        return;
-
-    fin.open("/proc/cmdline");
-    while (std::getline(fin, buf, ' '))
-        if (buf.find("board_id") != std::string::npos)
-            break;
-    fin.close();
-
-    if (buf.find("S88536CA2") != std::string::npos) {
-        property_override("ro.product.model", "Redmi 4");
-        property_override("ro.vendor.product.model", "Redmi 4");
-    }
+    android::init::property_set("ro.hwui.texture_cache_size", "72");
+    android::init::property_set("ro.hwui.layer_cache_size", "48");
+    android::init::property_set("ro.hwui.r_buffer_cache_size", "8");
+    android::init::property_set("ro.hwui.path_cache_size", "32");
+    android::init::property_set("ro.hwui.gradient_cache_size", "1");
+    android::init::property_set("ro.hwui.drop_shadow_cache_size", "6");
+    android::init::property_set("ro.hwui.texture_cache_flushrate", "0.4");
+    android::init::property_set("ro.hwui.text_small_cache_width", "1024");
+    android::init::property_set("ro.hwui.text_small_cache_height", "1024");
+    android::init::property_set("ro.hwui.text_large_cache_width", "2048");
+    android::init::property_set("ro.hwui.text_large_cache_height", large_cache_height);
 }
 
-void vendor_load_properties() {
-    dalvikprop_t dprop;
-    check_device(&dprop);
-
-    property_set("dalvik.vm.heapstartsize", dprop.heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", dprop.heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", dprop.heapsize);
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", dprop.heapminfree);
-    property_set("dalvik.vm.heapmaxfree", dprop.heapmaxfree);
-
-    init_target_properties();
-}
